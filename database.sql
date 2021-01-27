6ddb2009b0671238d30755119700f8809ee44500
James Daniels <jrd656@gmail.com>
2021-01-27 16:23:28 +0000

    install and add cross-env for heroku hosting

--- database.sql
+++ database.sql
@@ -1,4 +1,5 @@
 CREATE DATABASE authtodo;
+--  CREATE EXTENSION IF NOT EXISTS "uuid-ossp"; 
 
 CREATE TABLE users(
   user_id uuid DEFAULT uuid_generate_v4(),
--- db.js
+++ db.js
@@ -1,11 +1,17 @@
 const Pool = require("pg").Pool;
 
-const pool = new Pool({
+let DATABASE_URL = {
   host: "localhost",
   user: "postgres",
   password: "password",
   port: 5432,
   database: "authtodo"
-});
+}
+
+if (process.env.NODE_ENV === 'production') {
+  DATABASE_URL = process.env.DATABASE_URL
+}
+
+const pool = new Pool(DATABASE_URL);
 
 module.exports = pool;
